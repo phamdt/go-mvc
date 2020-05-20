338ca132855bfd2bfb2ae270feef67ebca7fd85a
danny pham <pham.danny.t@gmail.com>
2020-05-20 12:00:04 -0700

    implemented initial oa app generation

--- controller.go
+++ controller.go
@@ -3,11 +3,9 @@ package gomvc
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 	"strings"
 
-	rice "github.com/GeertJohan/go.rice"
-	"github.com/aymerick/raymond"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/iancoleman/strcase"
 	"github.com/jinzhu/inflection"
@@ -21,51 +19,41 @@ type ControllerData struct {
 }
 
 func createControllerFromDefault(controllerData ControllerData, dest string) error {
-	box := rice.MustFindBox("templates")
-	tmplString, err := box.String("gin/controller.tmpl")
-	if err != nil {
-		return err
-	}
-	tmpl, err := raymond.Parse(tmplString)
-	if err != nil {
-		return err
-	}
-	testTmplString := box.MustString("tests/controller_test.tpl")
-	testTmpl, err := raymond.Parse(testTmplString)
-	if err != nil {
-		return err
-	}
-	raymond.RegisterHelper("whichAction", func(action string) string {
-		log.Println("looking for HTTP action partial", action)
-		if action == "" {
-			log.Println("blank action name provided")
-			return ""
-		}
-		return methodPartial(controllerData, action, "gin")
-	})
 	lowerName := strings.ToLower(controllerData.Name)
-	filepath := fmt.Sprintf("%s/%s.go", dest, lowerName)
-	log.Println("fp", filepath)
-	result := tmpl.MustExec(controllerData)
-	if err := createFileFromString(filepath, result); err != nil {
-		log.Println("error generating file for", filepath, err.Error())
+	controllerPath := fmt.Sprintf("%s/%s.go", dest, lowerName)
+	helpers := []TemplateHelper{
+		{
+			Name: "whichAction",
+			Function: func(action string) string {
+				if action == "" {
+					log.Println("blank action name provided")
+					return ""
+				}
+				return methodPartial(controllerData, action, "gin")
+			},
+		},
+		{
+			Name: "whichActionTest",
+			Function: func(action string) string {
+				return methodPartial(controllerData, action+"_test", "tests")
+			},
+		},
+	}
+	if err := createFileWithHelpers(
+		"gin/controller.tmpl", controllerData, controllerPath, helpers); err != nil {
 		return err
 	}
+	// generate controller http tests
+	testControllerPath := fmt.Sprintf("%s/%s_test.go", dest, lowerName)
+	if err := createFileWithHelpers(
+		"tests/controller_test.tpl", controllerData, testControllerPath, helpers); err != nil {
+		return err
+	}
+
 	// register the controller operations in the router
-	routerFilePath := fmt.Sprintf("%s/router.go", dest)
+	routerFilePath := filepath.Join(dest, "router.go")
 	AddActionViaAST(controllerData.Actions, routerFilePath, dest)
 
-	// generate controller http tests
-	raymond.RegisterHelper("whichActionTest", func(action string) string {
-		log.Println("looking for HTTP action test partial", action)
-		return methodPartial(controllerData, action+"_test", "tests")
-	})
-	testfilepath := fmt.Sprintf("%s/%s_test.go", dest, lowerName)
-	testResult := testTmpl.MustExec(controllerData)
-	if err := createFileFromString(testfilepath, testResult); err != nil {
-		log.Println("error generating file for", testfilepath, err.Error())
-		return err
-	}
 	return nil
 }
 
@@ -85,10 +73,9 @@ func OACreateControllerFiles(path string, pathItem *openapi3.PathItem, dest stri
 		log.Printf("No summary provided in API, defaulting to deriving name from path %s since we can't identify a name for the resource", path)
 		name = getControllerNameFromPath(path)
 	}
-	log.Printf("Preparing to generate controller files for %s", name)
-
+	log.Printf("Preparing to generate controller files for %s %s", name, path)
 	data := ControllerData{
-		Name:       inflection.Singular(name),
+		Name:       name,
 		PluralName: inflection.Plural(name),
 		Path:       path,
 		Actions:    []Action{},
@@ -98,7 +85,7 @@ func OACreateControllerFiles(path string, pathItem *openapi3.PathItem, dest stri
 	for method, op := range pathItem.Operations() {
 		// i don't remember what this filters
 		if op.OperationID == "" && op.Summary == "" {
-			log.Printf("no operation ID or summary. Excluding operation from the generated %s controller file\n", name)
+			log.Printf("No operation ID or summary. Excluding operation from the generated %s controller file\n", name)
 			continue
 		}
 		var handler string
@@ -111,14 +98,11 @@ func OACreateControllerFiles(path string, pathItem *openapi3.PathItem, dest stri
 			Method: method, Path: path, Handler: handler, Name: handler,
 		}
 		data.Actions = append(data.Actions, action)
-		spew.Dump(data)
 	}
 
-	err := createControllerFromDefault(data, dest)
-	if err != nil {
+	if err := createControllerFromDefault(data, dest); err != nil {
 		return err
 	}
-
-	log.Printf("created %s\n", name)
+	log.Printf("created controller actions for %s\n", path)
 	return nil
 }
---
+++ oa.go
@@ -0,0 +1,54 @@
+package gomvc
+
+import (
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// OA is the cli command that creates a router and controller functions from an
+// OpenAPI file
+var OA = &cobra.Command{
+	Use:   "oa",
+	Short: "Generate controllers from an OpenAPI yml file",
+	Run: func(cmd *cobra.Command, args []string) {
+		configDir, err := cmd.LocalFlags().GetString("config")
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		config := NewGoMVCConfig(configDir)
+
+		spec, err := cmd.LocalFlags().GetString("spec")
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		oa3 := LoadWithKin(spec)
+
+		dest, err := cmd.LocalFlags().GetString("dest")
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		createDirIfNotExists(dest)
+		ctrlDest := filepath.Join(dest, "controllers")
+		createDirIfNotExists(ctrlDest)
+
+		templateDir, _ := cmd.LocalFlags().GetString("templates")
+		CreateRouter(RouteData{}, "gin/router.tpl", ctrlDest)
+		for path, pathItem := range oa3.Paths {
+			path = strings.Trim(path, " ")
+			log.Printf("examining path, %s\n", path)
+			if config.IsBlacklisted(path) {
+				continue
+			}
+			if err := OACreateControllerFiles(path, pathItem, ctrlDest, templateDir); err != nil {
+				log.Fatalf("%s: %s", path, err.Error())
+			}
+		}
+
+	},
+}
