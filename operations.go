3dad82014506c63b0d9c57df9b0bf95de09f3f35
danny pham <pham.danny.t@gmail.com>
2020-05-20 11:54:22 -0700

    add more oa3 boilerplate

--- operations.go
+++ operations.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/iancoleman/strcase"
+	"github.com/jinzhu/inflection"
 )
 
 func isIndex(method string, path string) bool {
@@ -13,15 +14,14 @@ func isIndex(method string, path string) bool {
 	}
 
 	// TODO: better way to determine?
-	return !strings.HasSuffix(path, "}")
+	return !hasPathParameter(path)
 }
 
 func getControllerNameFromPath(path string) string {
 	pathParts := strings.Split(path, "/")
 	nonParams := []string{}
 	for _, part := range pathParts {
-		// if it's a param it'll have the format `{paramName}`
-		if !strings.HasSuffix(part, "}") {
+		if !hasPathParameter(part) {
 			nonParams = append(nonParams, part)
 		}
 	}
@@ -29,5 +29,14 @@ func getControllerNameFromPath(path string) string {
 	nameIndex := int(math.Max(0, float64(lastTwoIndex)))
 	nonParams = nonParams[nameIndex:]
 	name := strcase.ToCamel(strings.Join(nonParams, "_"))
+	lastPathPart := pathParts[len(pathParts)-1]
+	if hasPathParameter(lastPathPart) {
+		return inflection.Singular(name)
+	}
 	return name
+}
+
+func hasPathParameter(s string) bool {
+	// if it's a param it'll have the format `{paramName}`
+	return strings.HasSuffix(s, "}")
 }
