becd6ddf48afd1c1eb3dd31352085d9bb1ab2621
phamdt <phamdt@users.noreply.github.com>
2020-05-25 08:32:15 -0700

    add clarifications for application generation (#3)

--- application.go
+++ application.go
@@ -23,7 +23,7 @@ var Application = &cobra.Command{
 	Short: "Generate application files",
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
-			return errors.New("requires a name for your new application")
+			return errors.New("requires a name for your new application: `gomvc application [name of your application]`")
 		}
 		return nil
 	},
--- go.mod
+++ go.mod
@@ -11,6 +11,6 @@ require (
 	github.com/jinzhu/inflection v1.0.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v0.0.5
-	golang.org/x/tools v0.0.0-20200509030707-2212a7e161a5 // indirect
+	golang.org/x/tools v0.0.0-20200522201501-cb1345f3a375 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
