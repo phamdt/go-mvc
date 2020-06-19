3a80d4fc8ae1c67a5ecef447ef02134c7fccadd9
danny pham <pham.danny.t@gmail.com>
2020-06-19 16:56:11 -0700

    create commands with a command

--- root.go
+++ root.go
@@ -3,16 +3,16 @@ package gomvc
 import "github.com/spf13/cobra"
 
 var root = &cobra.Command{
-	Use:   "gomvc",
-	Short: "GoMVC is a CLI for generating and modifying golang MVC applications",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := cmd.Help(); err != nil {
-			panic(err)
-		}
-	},
+  Use:   "gomvc",
+  Short: "GoMVC is a CLI for generating and modifying golang MVC applications",
+  Run: func(cmd *cobra.Command, args []string) {
+    if err := cmd.Help(); err != nil {
+      panic(err)
+    }
+  },
 }
 
 // Root is the function called when running `gomvc`
 func Root() *cobra.Command {
-	return root
+  return root
 }
