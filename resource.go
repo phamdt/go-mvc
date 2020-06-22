855afa3e03b966b76c298d69af7b7cef410c352a
phamdt <pham.danny.t@gmail.com>
2020-06-22 11:45:23 -0700

    reorder dest setting in resource

--- resource.go
+++ resource.go
@@ -21,6 +21,17 @@ var resource = &cobra.Command{
 		return nil
 	},
 	Run: func(cmd *cobra.Command, args []string) {
+		dest, _ := cmd.LocalFlags().GetString("dest")
+		// if no destination path is provided, assume that generation happens in
+		// the current directory
+		if dest == "" {
+			dir, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
+			dest = dir
+		}
+
 		name := args[0]
 		log.Printf("preparing to create a new resource %s\n", name)
 		path := filepath.Join("/", strings.ToLower(name))
@@ -29,14 +40,6 @@ var resource = &cobra.Command{
 			PluralName: inflection.Plural(name),
 			Path:       path,
 			Actions:    NewCRUDActions(name),
-		}
-		dest, _ := cmd.LocalFlags().GetString("dest")
-		if dest == "" {
-			path, err := os.Getwd()
-			if err != nil {
-				panic(err)
-			}
-			dest = path
 		}
 
 		if err := createControllerFromDefault(controllerData, dest); err != nil {
